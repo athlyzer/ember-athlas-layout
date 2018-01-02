e7455814998852d0a7f28afbee7d02ba8116c12f
gossi <gossi@users.noreply.github.com>
2018-01-02 16:15:01 +0100

    set root url for gh-pages

--- tests/dummy/config/environment.js
+++ tests/dummy/config/environment.js
@@ -5,7 +5,7 @@ module.exports = function(environment) {
   let ENV = {
     modulePrefix: 'dummy',
     environment,
-    rootURL: '/',
+    rootURL: environment === 'production' ? '/ember-athlas-layout/' : '/',
     locationType: 'auto',
     EmberENV: {
       FEATURES: {
