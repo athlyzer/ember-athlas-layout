a740e8798f91fa87de48e78382d58e078a1adb32
gossi <gossi@users.noreply.github.com>
2018-01-11 00:26:52 +0100

    location type to hash

--- tests/dummy/config/environment.js
+++ tests/dummy/config/environment.js
@@ -6,7 +6,7 @@ module.exports = function(environment) {
     modulePrefix: 'dummy',
     environment,
     rootURL: environment === 'production' ? '/ember-athlas-layout/' : '/',
-    locationType: 'auto',
+	locationType: 'hash',
     EmberENV: {
       FEATURES: {
         // Here you can enable experimental features on an ember canary build
